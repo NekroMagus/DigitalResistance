1fe8c9f701d8d624f382f38e5fdf85286b18333a
NekroMagus <NekroMagus@mail.ru>
2019-09-15 13:50:05 +0300

    some changes

--- target/digital-resistance-1.0-SNAPSHOT/WEB-INF/classes/schema.sql
+++ target/digital-resistance-1.0-SNAPSHOT/WEB-INF/classes/schema.sql
@@ -35,8 +35,8 @@ CREATE TABLE users_in_room
 );
 
 
-INSERT INTO users values (1,'648a81315307a32bd079533c4f47d258d309869f', '+79604786259', 'Mitya');
-INSERT INTO users values (2, '46f60547e5b77d13468ef49e9fac10c08b66a644', '+79604786260','Alex');
-INSERT INTO users values (3, 'd4256f0f0337cb8f7281750868a737c1a90d0c0d', '+79608343455', 'Mark');
-INSERT INTO users values (4, 'ebcb7dd8cd7b888b0ab7092eabc1b870bb0a0cc9', '+79596455198','Artem');
-INSERT INTO users values (5, '884541e3ef964580e43e5aff1fb60f21a0c1da4c', '+78834498345', 'Igor');
+INSERT INTO users values (1,'648a81315307a32bd079533c4f47d258d309869f', '89604786259', 'Mitya');
+INSERT INTO users values (2, '46f60547e5b77d13468ef49e9fac10c08b66a644', '89604786260','Alex');
+INSERT INTO users values (3, 'd4256f0f0337cb8f7281750868a737c1a90d0c0d', '89608343455', 'Mark');
+INSERT INTO users values (4, 'ebcb7dd8cd7b888b0ab7092eabc1b870bb0a0cc9', '89596455198','Artem');
+INSERT INTO users values (5, '884541e3ef964580e43e5aff1fb60f21a0c1da4c', '88834498345', 'Igor');
